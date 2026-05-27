5dbdecd8823b023b42a6e81747db306eea1f8eea
Marvin Koch <marvin.info@gmx.at>
2026-05-27 15:53:46 +0200

    feature(linting): add prettier package

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,25 @@
-const js = require('@eslint/js')
-const globals = require('globals')
+const js = require("@eslint/js");
+const globals = require("globals");
+const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = [
   {
-    ignores: [
-      'node_modules/**',
-      'coverage/**'
-    ]
+    ignores: ["node_modules/**", "coverage/**"],
   },
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'commonjs',
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
       globals: {
         ...globals.node,
-        ...globals.jest
-      }
+        ...globals.jest,
+      },
     },
     rules: {
-      'no-console': 'off'
-    }
-  }
-]
+      "no-console": "off",
+    },
+  },
+  eslintConfigPrettier,
+];
