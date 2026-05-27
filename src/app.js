d671b928d0525b6316e24e05c3e5db9a9029c5ec
Marvin Koch <marvin.info@gmx.at>
2026-05-27 15:54:04 +0200

    test(formating): tried prettier formatting

--- src/app.js
+++ src/app.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const notesService = require('./notesService')
+const express = require("express");
+const notesService = require("./notesService");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.post('/api/notes', (request, response) => {
-  const note = notesService.addNote(request.body)
+app.post("/api/notes", (request, response) => {
+  const note = notesService.addNote(request.body);
 
   if (note.error) {
-    return response.status(400).json(note)
+    return response.status(400).json(note);
   }
 
-  return response.json(note)
-})
+  return response.json(note);
+});
 
-app.get('/api/notes', (req, res) => {
-  res.json(notesService.getNotes())
-})
+app.get("/api/notes", (req, res) => {
+  res.json(notesService.getNotes());
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notesService.deleteNote(id)
-  response.status(204).end()
-})
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notesService.deleteNote(id);
+  response.status(204).end();
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notesService.findNoteById(id)
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notesService.findNoteById(id);
 
   if (note) {
-    return response.json(note)
+    return response.json(note);
   }
 
-  return response.status(404).end()
-})
+  return response.status(404).end();
+});
 
-module.exports = app
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require('./app')
+const app = require("./app");
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
