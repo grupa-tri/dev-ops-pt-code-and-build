d16d86ebef7dd7a92f46a03efddfbfb450d86e96
Marvin Koch <marvin.info@gmx.at>
2026-05-27 15:48:10 +0200

    feature(linting): add eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const js = require('@eslint/js')
+const globals = require('globals')
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      'coverage/**'
+    ]
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      'no-console': 'off'
+    }
+  }
+]
